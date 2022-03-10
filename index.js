30e44a037e26630b9daf9c54f58da7fa596a5d8a
Mark Bien <mark.bien@outlook.com>
2022-03-10 11:09:10 +0800

    Create styles.css, modify index.html and index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Variables
+const scores = document.querySelectorAll('.scores');
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+const titleRound = document.querySelector('h1');
+const play = document.querySelector('h2');
+const p1 = scores[0];
+const p2 = scores[1];
 
 // Event Listeners
+buttons.forEach(button => button.addEventListener('click', selectChoice));
+play.addEventListener('click', reset);
 
 // Functions
 function computerPlay() {
@@ -22,50 +30,76 @@ function computerPlay() {
     }
 }
 
-function capitalizeFirstLetter(input){
-    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+// function capitalizeFirstLetter(input) {
+//     return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+// }
+
+function playGame(playerInput, computerInput) {
+    console.log(playerInput);
+    console.log(computerInput);
+    if (playerInput === "Rock" && computerInput === "Scissors") {
+        // playerScore++;
+        updateScore(p1, ++playerScore);
+        // return "You win! Rock beats Scissors";
+    } else if (playerInput === "Rock" && computerInput === "Paper") {
+        updateScore(p2, ++computerScore);
+        // computerScore++;
+        // return "You lose! Paper beats Rock";
+    } else if (playerInput === "Paper" && computerInput === "Rock") {
+        updateScore(p1, ++playerScore);
+        // playerScore++;
+        // return "You win! Paper beats Rock";
+    } else if (playerInput === "Paper" && computerInput === "Scissors") {
+        updateScore(p2, ++computerScore);
+        // computerScore++;
+        // return "You lose! Scissors beats Paper";
+    } else if (playerInput === "Scissors" && computerInput === "Paper") {
+        updateScore(p1, ++playerScore);
+        // playerScore++;
+        // return "You win! Scissors beats Paper";
+    } else if (playerInput === "Scissors" && computerInput === "Rock") {
+        updateScore(p2, ++computerScore);
+        // computerScore++;
+        // return "You lose! Rock beats Scissors";
+    }
+    // } else {
+    //     return "Tie!";
+    // }
 }
 
-function playGame(playerInput, computerInput){
-    if(playerInput === "Rock" && computerInput === "Scissors"){
-        playerScore++;
-        return "You win! Rock beats Scissors";
-    } else if(playerInput === "Rock" && computerInput === "Paper"){
-        computerScore++;
-        return "You lose! Paper beats Rock";
-    }  else if(playerInput === "Paper" && computerInput === "Rock"){
-        playerScore++;
-        return "You win! Paper beats Rock";
-    } else if(playerInput === "Paper" && computerInput === "Scissors"){
-        computerScore++;
-        return "You lose! Scissors beats Paper";
-    } else if(playerInput === "Scissors" && computerInput === "Paper"){
-        playerScore++;
-        return "You win! Scissors beats Paper";
-    } else if(playerInput === "Scissors" && computerInput === "Rock"){
-        computerScore++;
-        return "You lose! Rock beats Scissors";
-    } else {
-        return "Tie!";
-    }
+function selectChoice() {
+    const choice = this.dataset['choice'];
+
+    buttons.forEach(button => {
+        if (button == this) {
+            button.classList.add('expand');
+        } else {
+            button.classList.add('hide');
+        }
+    });
+
+    // battles the computer
+    playGame(choice, computerPlay());
+
+
+    // resets choices after 3 seconds
+    setTimeout(reset, 3000);
 }
 
-function game(){
-    playerInput = ["Rock", "Rock", "Scissors", "Rock", "Paper"];
-    for(i = 0; i < 5; i++){
-        console.log(playGame(playerInput[i], computerPlay()));
-        console.log("Player score: " + playerScore);
-        console.log("Computer score: " + computerScore);
-    }
-
-    if(playerScore > computerScore){
-        console.log("Player wins!");
-    } else if(playerScore < computerScore) {
-        console.log("Computer wins!");
-    } else {
-        console.log("Tie game!");
-    }
+function reset() {
+    this.textContent = 'Pick your choice!';
+    buttons.forEach(button => {
+        button.classList.remove('expand');
+        button.classList.remove('hide');
+    });
 }
 
-// Call Function
-game();
+// update scores
+function updateScore(player, score) {
+    player.textContent = score;
+    console.dir(player.textContent);
+}
+
+// test score update
+// updateScore(scores[0], playerScore + 1);
+// updateScore(scores[1], computerScore + 2);
